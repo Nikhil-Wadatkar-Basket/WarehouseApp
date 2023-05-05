756e2c7b761dfdb27464830ebd9b71c4293afb93
Nikhil Wadatkar <nikhil.wadatkar4693@gmail.com>
2023-05-04 22:11:46 -0400

    list component added

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+
+
+export default function EmployeeList() {
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        getEmployees()
+    }, []);
+
+    const getEmployees = () => {
+
+        
+            setEmployees(
+                
+                [
+                    {id:1,firstName:"Nana",lastName:"Deotale",email:"test@gmail.com"},
+                    {id:1,firstName:"Nikhil",lastName:"Wadatkar",email:"test1@gmail.com"},
+                    {id:1,firstName:"Ankur",lastName:"Wadatkar",email:"test2@gmail.com"},
+                    {id:1,firstName:"Dhanashri",lastName:"Wadatkar",email:"test@gmail.com"}
+                ]
+            )
+            
+        }
+
+
+    return (
+        <div className = "container">
+            
+            <h1 className = "text-center"> Employees List</h1>
+
+            <div className='container'>
+            <table className = "table table-striped  table-bordered">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>First Name</th>
+                        <th>Last</th>
+                        <th>Email</th>
+                    </tr>
+
+                </thead>
+                <tbody>
+                    {
+                        employees.map(
+                                employee =>
+                                <tr key = {employee.id}>
+                                    <td> {employee.id }</td>
+                                    <td> {employee.firstName }</td>
+                                    <td> {employee.lastName }</td>    
+                                    <td> {employee.email }</td>
+
+                                </tr>
+
+                        )
+                    }
+
+                </tbody>
+
+
+            </table>
+            </div>
+
+        </div>
+    );
+                };
+            
+
